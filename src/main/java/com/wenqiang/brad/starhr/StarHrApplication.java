080d88b9456af402699c617de11d943d8d26149c
quwenqiang <919216634@qq.com>
2022-06-11 19:36:17 +0800

    init spring boot

---
+++ src/main/java/com/wenqiang/brad/starhr/StarHrApplication.java
@@ -0,0 +1,13 @@
+package com.wenqiang.brad.starhr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarHrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StarHrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wenqiang/brad/starhr/StarHrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wenqiang.brad.starhr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarHrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
