f8f557756fdba77fa19cbc3e0f8e7620939a6750
李招财 <yolo.l@192.168.1.4>
2024-03-25 02:24:30 +0800

    test

---
+++ src/main/java/com/wenqiang/brad/starhr/application/Index.java
@@ -0,0 +1,30 @@
+package com.wenqiang.brad.starhr.application;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 首页控制器
+ * @author pan_junbiao
+ **/
+@Controller
+public class Index
+{
+    /**
+     * 首页
+     */
+    @RequestMapping("/index")
+    public String index(HttpServletRequest request)
+    {
+        //将用户信息保存到Request对象中
+        request.setAttribute("name","pan_junbiao的博客");
+        request.setAttribute("blog","https://blog.csdn.net/pan_junbiao");
+        request.setAttribute("remark","您好，欢迎访问 pan_junbiao的博客");
+
+        //返回首页
+        return "page/index/index";
+    }
+}
